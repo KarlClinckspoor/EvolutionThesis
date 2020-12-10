6816e8d0648f448c2b7474177602d173c9384962
KarlClinckspoor <karlclinckspoor@protonmail.com>
2020-12-10 19:38:06 -0300

    Added more regex to clean latex document

--- commit_stats.py
+++ commit_stats.py
@@ -22,7 +22,7 @@ def count_unique_words(text):
     return unique_words
 
 
-def counter_words(text: str) -> Counter:
+def create_Counter_obj(text: str) -> Counter:
     """Creates a dictionary (specifically a collections.Counter) that contains
     the most used words in a string.
 
@@ -32,11 +32,75 @@ def counter_words(text: str) -> Counter:
         Returns:
             Counter: Counter object with the counts of the words. All lowercase.
     """
-    # TODO: add restrictions to regex to remove {figure} and similar things.
-    word_regex = re.compile(r"(?<!\\)\b\w+\b")
+    # Regex translation: Find everything not escaped (preceded by \), not
+    # preceded by "{", not followed by "}", that's made by word characters (\w)
+    # between two word boundaries (\b).
+    word_regex = re.compile(r"(?<!\\)(?<!{)\b\w+\b(?!})")
     words = word_regex.findall(text.lower())
     word_counter = Counter(words)
     return word_counter
+
+
+def remove_comments(text: str) -> str:
+    """Removes comments from the text using a simple regex
+
+    Args:
+        text (str): The whole text string
+
+    Returns:
+        str: Text string removing everything after a %
+    """
+    # From start of the line, select everything until the first % (lazily), if
+    # it is not preceded by \ (i.e. escaped). Then group everything from % to
+    # the end of the line.
+    comment_regex = re.compile(r"(^.*?)((?<!\\)%.*$)")
+    splitted_text = text.split("\n")
+    for i, line in enumerate(splitted_text):
+        # Not a list comprehension to make debugging easier
+        cleaned_line = comment_regex.sub("\1", line)
+        splitted_text[i] = cleaned_line
+    return "\n".join(splitted_text)
+
+
+def remove_includegraphics(text: str) -> str:
+    # TODO
+    pass
+
+
+def remove_labels(text: str) -> str:
+    # TODO
+    pass
+
+
+def remove_index(text: str) -> str:
+    # TODO
+    pass
+
+
+def remove_equation_envs(text: str) -> str:
+    # TODO
+    equation_env_regex = re.compile(r"\\begin{equation}.*\\end{equation}")
+    equation_env_regex2 = re.compile(r"\\begin{equation*}.*\\end{equation*}")
+
+
+def remove_citations(text: str) -> str:
+    # TODO
+    pass
+
+
+def remove_unnumbered_equations(text: str) -> str:
+    # TODO
+    # Regex translation: anything surrounded by "$ ... $", that's at least 1
+    # character, and doesn't isn't "$$", which is the line equation
+    old_inline_eq_regex = re.compile(r"\$(?<!\$).+?(?!\$)\$")
+    # Regex translation: anything surrounded by "\(...\)", that's at least 1
+    # character, and doesn't isn't "$$", which is the line equation
+    new_inline_eq_regex = re.compile(r"\\\(.+?\\\)")
+    # Regex translation: anything surrounded by "$$ ... $$" that's at least 1
+    # character.
+    old_line_eq_regex = re.compile(r"\$\$.+?\$\$")
+    ## Regex translation: anything surrounded by "\[ ... \]" that's at least 1 character.
+    new_line_eq_regex = re.compile(r"\\\[.*?\\\]")
 
 
 def open_file(filepath: str) -> str:
@@ -86,8 +150,8 @@ def test():
     print(
         count_words(text),
         count_unique_words(text),
-        counter_words(text).most_common(5),
-        remove_common_words(counter_words(text)).most_common(20),
+        create_Counter_obj(text).most_common(5),
+        remove_common_words(create_Counter_obj(text)).most_common(20),
         sep="\n",
     )
 
@@ -97,14 +161,15 @@ def test2():
     files = glob.glob(str(path / "*.tex"))
     for file in files:
         text = open_file(file)
+        text = remove_comments(text)
         print("-" * 20)
         print(f"{file}".center(20))
         print("-" * 20)
         print(
             f"Número de palavras {count_words(text)}",
             f"Número de palavras únicas {count_unique_words(text)}",
-            # counter_words(text).most_common(5),
-            f"Palavras mais comuns {remove_common_words(counter_words(text)).most_common(20)}",
+            # create_Counter_obj(text).most_common(5),
+            f"Palavras mais comuns {remove_common_words(create_Counter_obj(text)).most_common(20)}",
             sep="\n",
         )
 
